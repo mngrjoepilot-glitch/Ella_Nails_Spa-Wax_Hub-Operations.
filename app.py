e9b063ab566f5efe757a68e9cdcf29a6498416c5
mngrjoepilot-glitch <mngrjoepilot@gmail.com>
2026-02-07 16:09:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_client():
         scopes=scopes,
     )
     return gspread.authorize(creds)
-SHEET_ID = st.secrets["144GXa99ETKAMXvY6i9IELvmF7gF0U07ta2Z-IbIEN38"]
+SHEET_ID = st.secrets["sheet_id"]
 
 # ---------- LOAD DATA ----------
 sh = gc.open_by_key(SHEET_ID)
